319e4f75854c05510bb853691f84badb142aca0d
jefflunt <jefflunt@gmail.com>
2023-11-04 00:34:59 -0500

    start

---
+++ lib/tiny_work_service.rb
@@ -0,0 +1,52 @@
+require 'tiny_tcp_service'
+
+# usage:
+#  s = TinyWorkService.new(1234)
+#  s.stop!
+class TinyWorkService
+  def initialize(port)
+    @service = TinyTCPService.new(port)
+    @service.msg_handler = self
+    @jobs = Queue.new
+
+    Thread.new do
+      loop do
+        break unless @service.running?
+
+        print "\rTinyWorkService #{@jobs.length.to_s.rjust(6)} jobs #{@service.num_clients.to_s.rjust(4)} workers\e[K"
+        sleep 0.5
+      end
+    end
+  end
+
+  # interface for TinyTCPService
+  def call(m)
+    raise TinyTCPService::BadClient.new("nil message") if m.nil?
+
+    case
+    when m[0] == '+'        # add a job to the queue
+      self << m[1..]
+      'ok'                  # ok, job received
+    when m[0] == '-'        # take a job from the queue
+      shift || ''
+    else
+      raise TinyTCPService::BadClient.new("Client sent invalid message: `#{m[..50]}'")
+    end
+  end
+
+  # enqueue a job
+  def <<(j)
+    @jobs << j
+  end
+
+  # return the first job in the work queue, if there is one present
+  # otherwise, return nil
+  def shift
+    return nil if @jobs.empty?
+    @jobs.shift
+  end
+
+  def stop!
+    @service.stop!
+  end
+end
---
+++ tiny_work_service.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = "tiny_work_service"
+  s.version     = "1.0.0"
+  s.description = "uses the tiny_tcp_service gem to implement a job queue"
+  s.summary     = "uses the tiny_tcp_service gem to implement a job queue"
+  s.authors     = ["Jeff Lunt"]
+  s.email       = "jefflunt@gmail.com"
+  s.files       = ["lib/tiny_work_service.rb"]
+  s.homepage    = "https://github.com/jefflunt/tiny_work_service"
+  s.license     = "MIT"
+
+  s.add_runtime_dependency 'tiny_tcp_service', [">= 0"]
+end
